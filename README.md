8a52faa7b31800391c27157e2e32b079312db852
Nikita Akimov <korchiy@yandex.ru>
2024-01-02 18:03:14 +0300

    update

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
-# 1d_vertical
+# 1D Vertical
 
 Blender add-on to search and select the vertical polygons of the mesh.
 
-**Installation**
-
+Installation
+-
 User Preferences - Add-ons - Install Add-on from File - select downloaded archive
 
-**Usage**
-
-Location: "3D_View" window - T-tab - "Vertical" panel
+Usage
+-
 
 Two buttons - different algorithms:
 - Vertical 0 - checks the length of the maximum projection of the edge of the polygon on the Z axis
 - Vertical 1 - checks the maximum projection of the whole polygon on the Z axis
 
-**Including**
+Location
+-
+3D_View window - T-tab - "Vertical" panel
 
+Including
+-
 You need the module vertical.py
 
 Register this module in your add-on (with calling the "register" function)
@@ -30,5 +33,8 @@ The default algorithm is number 1. To call the operator with the second algorith
 
 Version history:
 -
-**v.0.0.0.**
+1.0.0
+- Integrated to the NA 1D Tools
+
+0.0.0
 - Dev start
--- vertical.py
+++ vertical.py
@@ -11,24 +11,52 @@
 #   1.3 (2018.06.09) - improve - uv 90-deg rotation
 #   1.4 (2018.06.09) - improve - rotate not selected uv polygons
 
-bl_info = {
-    'name': 'vertical',
-    'category': 'Mesh',
-    'author': 'Nikita Akimov',
-    'version': (1, 4, 0),
-    'blender': (2, 79, 0)
-}
-
 import bpy
+from bpy.props import BoolProperty, EnumProperty, IntProperty, PointerProperty
+from bpy.types import Operator, Panel, PropertyGroup, WindowManager
+from bpy.utils import register_class, unregister_class
 import math
 
+bl_info = {
+    "name": "Vertical",
+    "description": "Search and select the vertical polygons of the mesh",
+    "author": "Nikita Akimov, Paul Kotelevets",
+    "version": (1, 0, 0),
+    "blender": (2, 79, 0),
+    "location": "View3D > Tool panel > 1D > Vertical",
+    "doc_url": "https://github.com/Korchy/1d_vertical",
+    "tracker_url": "https://github.com/Korchy/1d_vertical",
+    "category": "Mesh"
+}
 
-class Vertical(bpy.types.Operator):
+
+class Vertical:
+
+    @staticmethod
+    def ui(layout, context):
+        # ui panel
+        button = layout.operator('vertical.select', text='Vertical 0')
+        button.algorithm = 0
+        button = layout.operator('vertical.select', text='Vertical 1')
+        button.algorithm = 1
+        row = layout.row()
+        row.prop(context.window_manager.interface_vars, 'axis', expand=True)
+        row = layout.row()
+        row.prop(context.window_manager.interface_vars, 'rotate_uv')
+        row.prop(context.window_manager.interface_vars, 'rotate_uv_invert_selection')
+        row = layout.row()
+        row.prop(context.window_manager.interface_vars, 'rotate_origin', expand=True)
+
+
+class Vertical_OT_Select(Operator):
     bl_idname = 'vertical.select'
     bl_label = 'Vertical: Select'
     bl_options = {'REGISTER', 'UNDO'}
 
-    algorithm = bpy.props.IntProperty(name='algorithm', default=1)
+    algorithm = IntProperty(
+        name='algorithm',
+        default=1
+    )
     uv_rotation_angle = 90
 
     def execute(self, context):
@@ -43,9 +71,9 @@ class Vertical(bpy.types.Operator):
             self.selectVerticalPolygons(context, obj)
             if context.window_manager.interface_vars.rotate_uv:
                 if context.window_manager.interface_vars.rotate_origin == '0':
-                    UV.rotate_selection(obj, (0, 0), __class__.uv_rotation_angle)
+                    UV.rotate_selection(obj, (0, 0), self.uv_rotation_angle)
                 elif context.window_manager.interface_vars.rotate_origin == '1':
-                    UV.rotate_selection(obj, UV.selection_center(obj), __class__.uv_rotation_angle)
+                    UV.rotate_selection(obj, UV.selection_center(obj), self.uv_rotation_angle)
         context.scene.objects.active = active
         bpy.ops.object.mode_set(mode=mode)
         return {'FINISHED'}
@@ -64,23 +92,35 @@ class Vertical(bpy.types.Operator):
                 if self.algorithm == 0:
                     # сравнение максимальных длин проекций ребер на оси
                     for edge in polygon.edge_keys:
-                        lx = math.fabs(activeobjectdata.vertices[edge[0]].co[0] - activeobjectdata.vertices[edge[1]].co[0])
+                        lx = math.fabs(
+                            activeobjectdata.vertices[edge[0]].co[0] - activeobjectdata.vertices[edge[1]].co[0]
+                        )
                         mlx = lx if not mlx or lx > mlx else mlx
-                        ly = math.fabs(activeobjectdata.vertices[edge[0]].co[1] - activeobjectdata.vertices[edge[1]].co[1])
+                        ly = math.fabs(
+                            activeobjectdata.vertices[edge[0]].co[1] - activeobjectdata.vertices[edge[1]].co[1]
+                        )
                         mly = ly if not mly or ly > mly else mly
-                        lz = math.fabs(activeobjectdata.vertices[edge[0]].co[2] - activeobjectdata.vertices[edge[1]].co[2])
+                        lz = math.fabs(
+                            activeobjectdata.vertices[edge[0]].co[2] - activeobjectdata.vertices[edge[1]].co[2]
+                        )
                         mlz = lz if not mlz or lz > mlz else mlz
                 elif self.algorithm == 1:
                     # сравнение максимальной проекции полигона на ось Z с максимальными проекциями ребер на оси
                     z_max = z_min = None
                     for vertex_id in polygon.vertices:
-                        z_min = activeobjectdata.vertices[vertex_id].co[2] if not z_min or z_min > activeobjectdata.vertices[vertex_id].co[2] else z_min
-                        z_max = activeobjectdata.vertices[vertex_id].co[2] if not z_max or z_max < activeobjectdata.vertices[vertex_id].co[2] else z_max
+                        z_min = activeobjectdata.vertices[vertex_id].co[2] \
+                            if not z_min or z_min > activeobjectdata.vertices[vertex_id].co[2] else z_min
+                        z_max = activeobjectdata.vertices[vertex_id].co[2] \
+                            if not z_max or z_max < activeobjectdata.vertices[vertex_id].co[2] else z_max
                     mlz = math.fabs(z_max - z_min)
                     for edge in polygon.edge_keys:
-                        lx = math.fabs(activeobjectdata.vertices[edge[0]].co[0] - activeobjectdata.vertices[edge[1]].co[0])
+                        lx = math.fabs(
+                            activeobjectdata.vertices[edge[0]].co[0] - activeobjectdata.vertices[edge[1]].co[0]
+                        )
                         mlx = lx if not mlx or lx > mlx else mlx
-                        ly = math.fabs(activeobjectdata.vertices[edge[0]].co[1] - activeobjectdata.vertices[edge[1]].co[1])
+                        ly = math.fabs(
+                            activeobjectdata.vertices[edge[0]].co[1] - activeobjectdata.vertices[edge[1]].co[1]
+                        )
                         mly = ly if not mly or ly > mly else mly
                 # check vertical
                 if context.window_manager.interface_vars.axis == 'Z' and mlz >= mlx and mlz >= mly:
@@ -113,9 +153,9 @@ class UV:
             center = (x, y)
         return center
 
-    @staticmethod
-    def rotate_selection(obj, origin, angle):
-        rot = __class__.make_rotation_transformation(math.radians(angle), origin)
+    @classmethod
+    def rotate_selection(cls, obj, origin, angle):
+        rot = cls.make_rotation_transformation(math.radians(angle), origin)
         for polygon_index, polygon in enumerate(obj.data.polygons):
             if polygon.select != bpy.context.window_manager.interface_vars.rotate_uv_invert_selection:
                 for i, loop_index in enumerate(polygon.loop_indices):
@@ -133,8 +173,8 @@ class UV:
         return xform
 
 
-class InterfaceVars(bpy.types.PropertyGroup):
-    axis = bpy.props.EnumProperty(
+class InterfaceVars(PropertyGroup):
+    axis = EnumProperty(
         items=[
             ('X', 'X', 'X', '', 0),
             ('Y', 'Y', 'Y', '', 1),
@@ -142,17 +182,17 @@ class InterfaceVars(bpy.types.PropertyGroup):
         ],
         default='Z'
     )
-    rotate_uv = bpy.props.BoolProperty(
+    rotate_uv = BoolProperty(
         name='UVRotate',
         description='Rotate UV of selected polygons to 90 deg',
         default=False
     )
-    rotate_uv_invert_selection = bpy.props.BoolProperty(
+    rotate_uv_invert_selection = BoolProperty(
         name='InvertSelection',
         description='Invert selection of rotating polygons',
         default=False
     )
-    rotate_origin = bpy.props.EnumProperty(
+    rotate_origin = EnumProperty(
         items=[
             ('0', '0,0', '0,0', '', 0),
             ('1', 'Selection center', 'Selection center', '', 1)
@@ -161,7 +201,7 @@ class InterfaceVars(bpy.types.PropertyGroup):
     )
 
 
-class VerticalPanel(bpy.types.Panel):
+class VerticalPanel(Panel):
     bl_idname = 'vertical.panel'
     bl_label = 'Vertical'
     bl_space_type = 'VIEW_3D'
@@ -169,31 +209,28 @@ class VerticalPanel(bpy.types.Panel):
     bl_category = '1D'
 
     def draw(self, context):
-        button = self.layout.operator('vertical.select', text='Vertical 0')
-        button.algorithm = 0
-        button = self.layout.operator('vertical.select', text='Vertical 1')
-        button.algorithm = 1
-        row = self.layout.row()
-        row.prop(context.window_manager.interface_vars, 'axis', expand=True)
-        row = self.layout.row()
-        row.prop(context.window_manager.interface_vars, 'rotate_uv')
-        row.prop(context.window_manager.interface_vars, 'rotate_uv_invert_selection')
-        row = self.layout.row()
-        row.prop(context.window_manager.interface_vars, 'rotate_origin', expand=True)
+        Vertical.ui(
+            layout=self.layout,
+            context=context
+        )
 
 
-def register():
-    bpy.utils.register_class(Vertical)
-    bpy.utils.register_class(VerticalPanel)
-    bpy.utils.register_class(InterfaceVars)
-    bpy.types.WindowManager.interface_vars = bpy.props.PointerProperty(type=InterfaceVars)
+def register(ui=True):
+    register_class(Vertical_OT_Select)
+    register_class(InterfaceVars)
+    WindowManager.interface_vars = PointerProperty(
+        type=InterfaceVars
+    )
+    if ui:
+        register_class(VerticalPanel)
 
 
-def unregister():
-    del bpy.types.WindowManager.interface_vars
-    bpy.utils.unregister_class(InterfaceVars)
-    bpy.utils.unregister_class(VerticalPanel)
-    bpy.utils.unregister_class(Vertical)
+def unregister(ui=True):
+    if ui:
+        unregister_class(VerticalPanel)
+    del WindowManager.interface_vars
+    unregister_class(InterfaceVars)
+    unregister_class(Vertical_OT_Select)
 
 
 if __name__ == '__main__':
